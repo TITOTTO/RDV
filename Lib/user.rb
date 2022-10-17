c9170b39ccd37a222005360d3f67ba848891e027
TITOTTO <kuaker26@gmail.com>
2022-10-17 13:34:54 +0200

    MAJ

--- Lib/user.rb
+++ Lib/user.rb
@@ -2,33 +2,30 @@ require "pry"
 
 class User
     attr_accessor :email, :age
-    @@alluser = Hash.new
+    @@alluser = []
 
     def initialize(mail, age)
         @email = mail
         @age = age
-        @@alluser[mail] = age
+        @@alluser << self
     end
 
     def self.all
-        return @@alluser
+        @@alluser
     end
 
-    # def self.find_by_email(mail)
-    #     return true if @@alluser.include?(mail)
-    #     return "ta mere"
-    # end
-
-    def self.find_by_email(mail)
-        @@alluser.each do |x, y|
-            if x == mail
-                return User.new(x, y)     
-            end
-            return "Cette utilisateur n'existe pas"
+    def self.find_by_email(email)
+        @@allusers.each do |user|
+          if user.email == email
+            return user
+          end
         end
-    end
+        puts "aucun utilisateur n'a cet email"
+        return false
+      end
 
     def monage
         return "Voici l'age du User trouvé : #{@age}"
     end
 end
+binding.pry
